b45402577dd0ca13209f50c69197c340a0f5aa53
Sang-Gnas <nmsangcntt2211041@student.ctuet.edu.vn>
2025-03-28 18:15:19 +0700

    sang

--- tts-shop/src/views/App.js
+++ tts-shop/src/views/App.js
@@ -3,12 +3,14 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./Home";
 import ProductDetail from "./ProductDetail";
 import "bootstrap-icons/font/bootstrap-icons.css";
+import Login from "./Login";
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="home" element={<Home />} />
         <Route path="/productdetail" element={<ProductDetail />} />
+        <Route path="Login" element={<Login />} />
       </Routes>
     </Router>
   );
