7ddacec9a2b40841b956b61063a86a8e3ba0d89a
Thoaidz23 <mthoai733@gmail.com>
2025-03-29 09:42:29 +0700

    capnhat nav

--- tts-shop/src/views/Home.js
+++ tts-shop/src/views/Home.js
@@ -1,15 +1,31 @@
+import React from "react";
 import Navbar from "../component/NavBar";
-import '../styles/home.scss';  // nếu file nằm trong src/styles/
-import '../styles/ProductDetail.scss';
+import MenuBar from "../component/MenuBar";
+import "../styles/home.scss";
+import "../styles/ProductDetail.scss";
+import '../styles/index.scss';
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.bundle.min";
 function Home() {
-    return(
-        <div>
-      <Navbar />  {/* Header sẽ hiển thị trên đầu */}
-      <main>
-        <h2>Chào mừng đến với Shop Điện Thoại</h2>
-        <p>Trang chủ hiển thị danh sách sản phẩm...</p>
+  return (
+    <div>
+      {/* Navbar */}
+      <Navbar />
+      {/* MenuBar */}
+      <div className="menu-wrapper">
+        <MenuBar />
+      </div>
+      {/* Nội dung chính */}
+      <div className="container">
+      <main className="container-content">
+         {/* Banner quảng cáo */}
+      <div className="banner">
+        <img src="/path/to/banner.jpg" alt="Banner Quảng Cáo" />
+      </div>
       </main>
+      </div>
     </div>
-    )
+  );
 }
+
 export default Home;
