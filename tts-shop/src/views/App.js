7a24c280de97de68aa47c19073083acb4adbbd7f
Sang-Gnas <nmsangcntt2211041@student.ctuet.edu.vn>
2025-04-05 16:09:16 +0700

    sang

--- tts-shop/src/views/App.js
+++ tts-shop/src/views/App.js
@@ -1,35 +1,25 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./Home";
-import "bootstrap-icons/font/bootstrap-icons.css";
 import Login from "./Login";
-<<<<<<< HEAD
-=======
 import Register from "./Register";
-import NewPassword from "./NewPassword"
-import ForgetPassword from "./ForgetPassword"
->>>>>>> ea016cc7e640f96546117fa8646a2713a3e16249
+import NewPassword from "./NewPassword";
+import ForgetPassword from "./ForgetPassword";
+import ProductDetail from "./ProductDetail"; // Đừng quên import nếu bạn có file này
+import "bootstrap-icons/font/bootstrap-icons.css";
 
 function App() {
   return (
     <Router>
       <Routes>
-<<<<<<< HEAD
-        {/* Trang mặc định */}
         <Route path="/" element={<Home />} />
-
-        {/* Nếu vẫn muốn có cả /home */}
         <Route path="/home" element={<Home />} />
-        
+
         <Route path="/login" element={<Login />} />
-=======
-        <Route path="home" element={<Home />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/newpassword" element={<NewPassword />} />
+        <Route path="/forgetpassword" element={<ForgetPassword />} />
         <Route path="/productdetail" element={<ProductDetail />} />
-        <Route path="Login" element={<Login />} />
-        <Route path="Register" element={<Register />} />
-        <Route path="NewPassword" element={<NewPassword />} />
-        <Route path="ForgetPassword" element={<ForgetPassword />} />
->>>>>>> ea016cc7e640f96546117fa8646a2713a3e16249
       </Routes>
     </Router>
   );
