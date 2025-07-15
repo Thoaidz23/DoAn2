aece70fccf5c044d93abbef558e6fa89193f7930
Truong Thanh Thuong <truongthuong1512@gmail.com>
2025-07-15 19:40:11 +0700

    Thuong

--- tts-shop/src/server/admin/routes/commentRoutes.js
+++ tts-shop/src/server/admin/routes/commentRoutes.js
@@ -1,14 +1,14 @@
-const express = require("express");
-const router = express.Router();
-const commentController = require("../controllers/commentController");
+// const express = require("express");
+// const router = express.Router();
+// const commentController = require("../controllers/commentController");
 
-// GET: Lấy tất cả bình luận theo sản phẩm
-router.get("/:id_group_product", commentController.getCommentsByProduct);
+// // GET: Lấy tất cả bình luận theo sản phẩm
+// router.get("/:id_group_product", commentController.getCommentsByProduct);
 
-// POST: Thêm bình luận hoặc phản hồi
-router.post("/", commentController.addComment);
+// // POST: Thêm bình luận hoặc phản hồi
+// // router.post("/", commentController.addComment);
 
-// DELETE: Xóa bình luận (và các phản hồi)
-router.delete("/:id", commentController.deleteComment);
+// // DELETE: Xóa bình luận (và các phản hồi)
+// router.delete("/:id", commentController.deleteComment);
 
-module.exports = router;
+// module.exports = router;
