dd622d3390332fd9e6e497f87a84ec157e47f0d1
Truong Thanh Thuong <truongthuong1512@gmail.com>
2025-08-08 21:05:50 +0700

    T

--- tts-shop/src/server/admin/controllers/orderController.js
+++ tts-shop/src/server/admin/controllers/orderController.js
@@ -32,57 +32,6 @@ const getOrders = (req, res) => {
 };
 
 // Lấy chi tiết đơn hàng theo code_order
-// const getOrderByCode = (req, res) => {
-//   const { code } = req.params;
-
-//   const query = `
-//     SELECT 
-//       o.id_order, o.code_order, o.total_price, o.status, o.date, 
-//       o.name_user, o.address, o.phone,
-//       u.id_user, u.name AS user_name, u.email,
-//       od.id_order_detail, od.quantity_product,
-//       p.id_product, p.price,
-//       gp.name_group_product, gp.image,
-//       ram.name_ram, rom.name_rom, color.name_color,
-//       pi.method, pi.paystatus
-//     FROM tbl_order o
-//     JOIN tbl_user u ON o.id_user = u.id_user
-//     JOIN tbl_order_detail od ON o.code_order = od.code_order
-//     JOIN tbl_product p ON od.id_product = p.id_product
-//     JOIN tbl_group_product gp ON p.id_group_product = gp.id_group_product
-//     LEFT JOIN tbl_ram ram ON p.id_ram = ram.id_ram
-//     LEFT JOIN tbl_rom rom ON p.id_rom = rom.id_rom
-//     LEFT JOIN tbl_color color ON p.id_color = color.id_color
-//     LEFT JOIN tbl_payment_infor pi ON o.code_order = pi.code_order
-//     WHERE o.code_order = ?;
-//   `;
-
-//   connection.query(query, [code], (err, results) => {
-//     if (err) {
-//       console.error("Lỗi truy vấn:", err);
-//       return res.status(500).json({ error: "Lỗi máy chủ" });
-//     }
-
-//     if (results.length === 0) {
-//       return res.status(404).json({ message: "Đơn hàng không tồn tại" });
-//     }
-
-//     const order = results[0];
-//     const products = results.map(row => ({
-//       name_group_product: row.name_group_product,
-//       name_ram: row.name_ram,
-//       name_rom: row.name_rom,
-//       name_color: row.name_color,
-//       quantity_product: row.quantity_product,
-//       price: row.price,
-//       image: row.image
-//     }));
-
-//     res.json({ ...order, products });
-//   });
-// };
-
-// Lấy chi tiết đơn hàng theo code_order
 const getOrderByCode = (req, res) => {
   const { code } = req.params;
 
